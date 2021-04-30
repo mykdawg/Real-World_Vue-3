13f5b49e9edf82dad4f8d3c02793446a73750217
mj_wheelwright <6102553N1662h>
2021-04-29 20:28:28 -0700

    code changes

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,10 +1,14 @@
 <template>
-  <router-link class="event-link" :to="{ name: 'EventDetails', params: { id: event.id }}">
+  <router-link
+    class="event-link"
+    :to="{ name: 'EventDetails', params: { id: event.id } }"
+  >
     <div class="event-card">
       <span>@{{ event.time }} on {{ event.date }}</span>
       <h4>{{ event.title }}</h4>
     </div>
-  </router-link> |  
+  </router-link>
+  |
 </template>
 
 <script>
